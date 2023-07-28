343f089b4ee8e9a8b58da3f0b3984c4a7fddf777
kelvinVargasUG <joel.vargas.ch@hotmail.com>
2023-07-28 14:35:26 -0500

    ultimo commit

--- login-pantilla-backend/src/main/java/com/sasf/loginpantillabackend/Controller/Usuario/UsuarioControlador.java
+++ login-pantilla-backend/src/main/java/com/sasf/loginpantillabackend/Controller/Usuario/UsuarioControlador.java
@@ -1,5 +1,6 @@
 package com.sasf.loginpantillabackend.Controller.Usuario;
 
+import java.util.Arrays;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -55,6 +56,44 @@ public class UsuarioControlador {
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> getUsuarioById(@Valid @PathVariable Integer id) {
         return ResponseEntity.ok(usuarioService.getUsuarioById(id));
+    }
+
+    @GetMapping("/prueba")
+    @PreAuthorize("hasAuthority('Admin')")
+    public void prueba() {
+        int[] myArray = {1, 2, -1, 1, 0, 1, 2, -1, -1, -2};
+        char[][] grid = new char[4][4];
+
+        // Inicializar la matriz con 'O'
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = 'O';
+            }
+        }
+
+        int x = 0; // Posición inicial en el eje x
+        int y = 0; // Posición inicial en el eje y
+
+        // Realizar los movimientos
+        for (int i = 0; i < myArray.length; i += 2) {
+            int dx = myArray[i];     // Movimiento en el eje horizontal
+            int dy = myArray[i + 1]; // Movimiento en el eje vertical
+
+            // Actualizar la posición de la X
+            x = Math.max(0, Math.min(x + dx, 3)); // Limitar la posición dentro del rango (0-3)
+            y = Math.max(0, Math.min(y + dy, 3)); // Limitar la posición dentro del rango (0-3)
+
+            // Colocar la X en la posición actual
+            grid[y][x] = 'X';
+        }
+
+        // Imprimir la matriz resultante
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
     }
 
 }
